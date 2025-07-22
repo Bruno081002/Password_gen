ae0e7898e0b5daafd2f4e2f254359689bd4f9482
Bruno Fortes <brunofortes2002@gmail.com>
2025-07-22 02:30:56 +0100

    Starting

---
+++ PasswordGen.py
@@ -0,0 +1,17 @@
+import random
+import string
+import tkinter as tk
+from tkinter import *
+
+def password_generate():
+    letters = string.ascii_letters
+    digit = string.digits
+    pontuantion = string.punctuation
+    whitespaces = string.whitespace
+
+        
+    
+
+
+password_generate()
+
